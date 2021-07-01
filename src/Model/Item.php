2bf028cdcd51ed6039795e4fbf656c8d3c2d0157
Erikvo22 <erikvieraol22@gmail.com>
2021-07-01 17:40:41 +0100

    - In "Item.php", the type of each variable is indicated.
    - Getters and setters are generated.

--- src/Model/Item.php
+++ src/Model/Item.php
@@ -4,9 +4,20 @@ namespace Runroom\GildedRose\Model;
 
 class Item {
 
-    public $name;
-    public $sell_in;
-    public $quality;
+    /**
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * @var int
+     */
+    public int $sell_in;
+
+    /**
+     * @var int
+     */
+    public int $quality;
 
     function __construct($name, $sell_in, $quality) {
         $this->name = $name;
@@ -14,8 +25,58 @@ class Item {
         $this->quality = $quality;
     }
 
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSellIn(): int
+    {
+        return $this->sell_in;
+    }
+
+    /**
+     * @param int $sell_in
+     */
+    public function setSellIn(int $sell_in): void
+    {
+        $this->sell_in = $sell_in;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuality(): int
+    {
+        return $this->quality;
+    }
+
+    /**
+     * @param int $quality
+     */
+    public function setQuality(int $quality): void
+    {
+        $this->quality = $quality;
+    }
+
+
+
     public function __toString() {
-        return "{$this->name}, {$this->sell_in}, {$this->quality}";
+        return "{$this->getName()}, {$this->getSellIn()}, {$this->getQuality()}";
     }
 
 }
